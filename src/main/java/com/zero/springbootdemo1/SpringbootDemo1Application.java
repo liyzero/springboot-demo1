ebbd813d83a43517efc58afbc98f62811bbb8e4e
zorro <zorro.li@sunvalley.com.cn>
2018-02-11 18:14:29 +0800

    增加sayHello接口

--- src/main/java/com/zero/springbootdemo1/SpringbootDemo1Application.java
+++ src/main/java/com/zero/springbootdemo1/SpringbootDemo1Application.java
@@ -3,6 +3,12 @@ package com.zero.springbootdemo1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 类或方法的功能描述 :TODO
+ *
+ * @author: zorro.li
+ * @date: 2018-02-11 12:20
+ */
 @SpringBootApplication
 public class SpringbootDemo1Application {
 
--- src/main/java/com/zero/springbootdemo1/controler/HelloControler.java
+++ src/main/java/com/zero/springbootdemo1/controler/HelloControler.java
@@ -3,16 +3,44 @@ package com.zero.springbootdemo1.controler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 类或方法的功能描述 :TODO
  *
- * @author: zorro.li
- * @date: ${date}${time}
- */
+ * @param
+ * @author zorro.li
+ * @date 2018/2/11 17:50
+ * @return
+ */  
 @RestController
 public class HelloControler {
     @RequestMapping("/")
     public String index() {
         return "Greetings from 111 Spring Boot!";
+    }
+
+    @RequestMapping("/hello")
+    /**
+     *
+     *
+     * @author zorro.li
+     * @date 2018/2/11 17:41
+     * @param []
+     * @return java.lang.String
+     */
+    public String sayHello() {
+        return "Greetings from 111 Spring Boot!";
+    }
+
+    /**
+     * @param [str] count]
+     * @return java.util.Map
+     * @author zorro.li
+     * @date 2018/2/11 17:44
+     */
+    public Map getParam(String str, int count) {
+        return null;
     }
 }
